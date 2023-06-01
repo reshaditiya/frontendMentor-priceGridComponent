06d03eb50643b0ebc24fc293f899751a72bd9e01
reshaditiya <reshaditiya@gmail.com>
2023-06-01 19:58:09 +0700

    layout fix

---
+++ script.js
@@ -0,0 +1,17 @@
+// UNOCSS
+window.__unocss = {
+	theme: {
+		colors: {
+			primaryCyan: "hsl(179, 62%, 43%)",
+			primaryYellow: "hsl(71, 73%, 54%)",
+			neutralGray: "hsl(204, 43%, 93%)",
+			neutralBlue: "hsl(218, 22%, 67%)",
+		},
+		fontFamily: {
+			karla: "Karla",
+		},
+	},
+	shortcuts: {
+		"text-sub-title": "text-4.5 font-700 -tracking-0.06",
+	},
+}
