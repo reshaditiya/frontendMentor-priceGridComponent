d328cd953135abf22738598fc99e25742d6ed6a0
reshaditiya <reshaditiya@gmail.com>
2023-06-01 20:03:12 +0700

    layout fix

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 window.__unocss = {
 	theme: {
 		colors: {
+			primaryLightCyan: "#4abebd",
 			primaryCyan: "hsl(179, 62%, 43%)",
 			primaryYellow: "hsl(71, 73%, 54%)",
 			neutralGray: "hsl(204, 43%, 93%)",
